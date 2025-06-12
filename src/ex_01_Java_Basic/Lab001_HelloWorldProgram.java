c23ee2a0479aa991d9e2dccb4ab282b0bd6a1c9a
Nikita <ochaninikita09@gmail.com>
2025-06-12 15:52:19 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -0,0 +1,6 @@
+package ex_01_Java_Basic;
+public class Lab001_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello I am writing a Java Program");
+    }
+}
