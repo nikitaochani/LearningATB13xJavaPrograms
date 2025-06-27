f231dfa271618e0fa0799773ff7eac24ccda1ce0
Nikita <ochaninikita09@gmail.com>
2025-06-27 18:53:19 +0530

    Test Commit

--- src/ex_01_Java_Basic/Lab003_Keyword_Identifiers.java
+++ src/ex_01_Java_Basic/Lab003_Keyword_Identifiers.java
@@ -3,7 +3,7 @@ package ex_01_Java_Basic;
 public class Lab003_Keyword_Identifiers {
     public static void main(String[] args) {
         System.out.println("Let's learn about Keywords & Identifiers");
-        //Keywords are reserved words & cannot be used while creating any class, method, variables, etc.
+        //Keywords are reserved words, they are defined in lower case & cannot be used while creating any class, method, variables, etc.
         System.out.println("package, public, class, static, void are all keywords here");
         //Identifiers are names that will be used to identify the programming elements like classes, methods, variables, etc uniquely.
         System.out.println("ex_01_Java_Basic, Lab003_Keyword_Identifiers, args are all identifiers here");
