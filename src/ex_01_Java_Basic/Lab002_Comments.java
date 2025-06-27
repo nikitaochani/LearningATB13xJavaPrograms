2dfadba8071f4d2363ccb636394f67274a25a231
Nikita <ochaninikita09@gmail.com>
2025-06-27 17:39:00 +0530

    Added 2 new files to learn comments, keywords & identifiers in Java

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        //Comment
+        System.out.println("This is a comment");
+        /*
+        Multi-line comment
+        Multi-line comment
+        Multi-Line comment
+         */
+        System.out.println("This is a multi-line comment");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab003_Keyword_Identifiers.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Keyword_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Let's learn about Keywords & Identifiers");
+        //Keywords are reserved words & cannot be used while creating any class, method, variables, etc.
+        System.out.println("package, public, class, static, void are all keywords here");
+        //Identifiers are names that will be used to identify the programming elements like classes, methods, variables, etc uniquely.
+        System.out.println("ex_01_Java_Basic, Lab003_Keyword_Identifiers, args are all identifiers here");
+
+    }
+}
