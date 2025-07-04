f96025cd2bcce088d7e25a6334e33dbfaca5a379
Nikita <ochaninikita09@gmail.com>
2025-07-04 17:00:33 +0530

    Assignment to print Table of 9 using Printf()

---
+++ src/ex_01_Java_Basic/Lab006_Table_of_9_using_printf.java
@@ -0,0 +1,36 @@
+package ex_01_Java_Basic;
+public class Lab006_Table_of_9_using_printf {
+    public static void main(String[] args) {
+        int i = 9;
+        int j = 1;
+        int k = 2;
+        int l = 3;
+        int m = 4;
+        int n = 5;
+        int o = 6;
+        int p = 7;
+        int q = 8;
+        int r = 9;
+        int s = 10;
+        System.out.printf("%d * %d = %d", i,j, i*j);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,k, i*k);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,l, i*l);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,m, i*m);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,n, i*n);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,o, i*o);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,p, i*p);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,q, i*q);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,r, i*r);
+        System.out.println();
+        System.out.printf("%d * %d = %d", i,s, i*s);
+        System.out.println();
+    }
+}
