e7cfeea6bdc046596404330fa89fe0dcbf3cf778
Nikita <ochaninikita09@gmail.com>
2025-07-04 15:12:29 +0530

    Data Types & Variables

---
+++ src/ex_01_Java_Basic/Lab004_DataTypes_Variables.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class Lab004_DataTypes_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 50;
+        char d = 'h';
+        short s = 34;
+        long t = 10878900L;
+        float f = 10.67f;
+        double j = 12.87;
+        System.out.println("Value of j is " +j);
+        System.out.println("Value of b is " +b);
+        System.out.println("Value of t is " +t);
+        System.out.println("Value of f is " +f);
+        System.out.println("Value of d is " +d);
+    }
+}
